a6fb908e52e3a2693f801be8512ff8062694480f
jxnipat <Jetnipat.prc@gmail.com>
2023-08-13 17:22:05 +0700

    HW3.1

--- Homework/HW03/hw03-1-codex.cpp
+++ Homework/HW03/hw03-1-codex.cpp
@@ -1,18 +1,33 @@
-//TODO: #8 Arrange this C coding into CODEX pattern.
+// Including necessary header files
 #include <stdio.h>
 #include <conio.h>
-int main(){
-    int a=10;
-int b=20,c=30,d=40; 
-printf("This is\n %d + %d = %d ",a,b,a+b);
-    for(int i=0;i<d-30;i++){
-  if(i<30){
- printf("Here\n");
-   if(i==8){
-    printf("This is 8.\n");
+
+// Main function
+int main() {
+    // Variable declarations
+    int a = 10;
+    int b = 20, c = 30, d = 40;
+
+    // Printing the initial message
+    printf("This is\n %d + %d = %d ", a, b, a + b);
+
+    // Loop to iterate through values
+    for (int i = 0; i < d - 30; i++) {
+        // Conditional check for i
+        if (i < 30) {
+            printf("Here\n");
+
+            // Nested conditional check for i
+            if (i == 8) {
+                printf("This is 8.\n");
+            }
+        }
+        
+        // Printing "ok" at each iteration
+        printf("ok\n");
     }
-  }
-  printf("ok\n");
+
+    // Returning 0 to indicate successful execution
+    return 0;
 }
-return 0;
-}
+
